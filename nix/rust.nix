01f4f885e3e9bd20b13cb351c8b0bc408a405db7
Anders Christiansen Sørby <anders@sorby.xyz>
2021-01-18 16:42:02 +0100

    Fix Nix Build

--- nix/rust.nix
+++ nix/rust.nix
@@ -4,7 +4,7 @@ let
   pkgs =
     import sources.nixpkgs { overlays = [ (import sources.nixpkgs-mozilla) ]; };
   channel = "nightly";
-  date = "2020-03-08";
+  date = "2021-01-17";
   targets = [ "wasm32-unknown-unknown" ];
   chan = pkgs.rustChannelOfTargets channel date targets;
 in chan
