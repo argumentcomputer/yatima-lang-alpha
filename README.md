8fecb3415f4fc59eda76042a7574b4c2af4b4a9c
Anders Christiansen Sørby <anders@sorby.xyz>
2021-01-18 18:01:28 +0100

    Add nix.dev setup and shellHook

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# yatima
+# Yatima
 A programming language for the decentralized web
+
+# Development environment
+
+Set up dev envoronemnt assuming [nix](https://nixos.org) and direnv are installed.
+```bash
+direnv allow
+```
--- shell.nix
+++ shell.nix
@@ -1,10 +1,9 @@
-let
-  sources = import ./nix/sources.nix;
-  rust = import ./nix/rust.nix { inherit sources; };
-  pkgs = import sources.nixpkgs {};
-in
-pkgs.mkShell {
-  buildInputs = [
-    rust pkgs.rustfmt pkgs.wasm-pack pkgs.wasm-bindgen-cli
-  ];
+{ project ? import ./nix { }
+}:
+
+project.pkgs.mkShell {
+  buildInputs = builtins.attrValues project.devTools;
+  shellHook = ''
+    ${project.ci.pre-commit-check.shellHook}
+  '';
 }
