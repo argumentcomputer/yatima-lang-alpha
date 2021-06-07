0626ea947bc953b24520239f298f76130c92f354
Anders Christiansen Sørby <anders@sorby.xyz>
2021-06-07 15:44:52 +0200

    Fix copy and paste functionality

--- web/src/repl.rs
+++ web/src/repl.rs
@@ -38,7 +38,6 @@ use xterm_js_rs::{
     search::SearchAddon,
     web_links::WebLinksAddon,
   },
-  Event,
   OnKeyEvent,
   Terminal,
   TerminalOptions,
@@ -67,8 +66,12 @@ const KEY_L: u32 = 76;
 const CLEAR_LINE: &str = "\x1b[2K";
 const CURSOR_LEFT: &str = "\x1b[D";
 const CURSOR_RIGHT: &str = "\x1b[C";
-// const CURSOR_UP: &str = "\x1b[A";
-// const CURSOR_DOWN: &str = "\x1b[B";
+const CURSOR_UP: &str = "\x1b[A";
+const CURSOR_DOWN: &str = "\x1b[B";
+const BACKSPACE: &str = "\x1b[H";
+const RETURN: &str = "\x1b[M";
+const LINEFEED: &str = "\x1b[J";
+
 
 fn set_column(term: &Terminal, col: u32) {
   term.write(&format!("\x1b[{}G", col));
@@ -100,6 +103,55 @@ impl Default for ShellState {
       line: String::new(),
       cursor_col: 0,
       history_index: 0,
+    }
+  }
+}
+
+impl Repl for WebRepl {
+  fn readline(&mut self, _prompt: &str) -> Result<String, ReplError> {
+    Ok(self.shell_state.lock().unwrap().clone().line)
+  }
+
+  fn get_defs(&self) -> Arc<Mutex<Defs>> { self.defs.clone() }
+
+  fn get_store(&self) -> Rc<dyn Store> { self.store.clone() }
+
+  fn println(&self, s: String) { self.terminal.writeln(s.as_str()); }
+
+  fn load_history(&mut self) {
+    let window =
+      web_sys::window().expect("should have a window in this context");
+    let storage =
+      window.local_storage().expect("should have local storage").unwrap();
+    if let Ok(Some(text)) = storage.get("history.txt") {
+      let split: Vec<&str> = text.split("\n").collect();
+      for s in split {
+        self.history.push_front(s.to_owned());
+      }
+      // Blank line is the current line
+      self.history.push_front("".to_owned());
+      log("History loaded");
+    }
+    else {
+      log("Could not load history");
+    }
+  }
+
+  fn add_history_entry(&mut self, s: &str) { self.history.push_front(s.to_owned()); }
+
+  fn save_history(&mut self) {
+    let window =
+      web_sys::window().expect("should have a window in this context");
+    let storage =
+      window.local_storage().expect("should have local storage").unwrap();
+
+    let v: Vec<String> = self.history.clone().into();
+    let history = v.join("\n");
+    if let Ok(()) = storage.set("history.txt", &history) {
+      log("History saved");
+    }
+    else {
+      log("Could not save history");
     }
   }
 }
@@ -156,6 +208,10 @@ impl WebRepl {
     WebRepl { terminal, defs, shell_state, store, history: VecDeque::new() }
   }
 
+  pub fn get_terminal(&self) -> Terminal {
+    self.terminal.clone().dyn_into().unwrap()
+  }
+
   pub fn handle_event(&mut self, e: OnKeyEvent) {
     let shell_state = self.shell_state.lock().unwrap();
     let mut cursor_col = shell_state.cursor_col.clone();
@@ -164,7 +220,7 @@ impl WebRepl {
 
     // We are done reading from the shell_state
     drop(shell_state);
-    let term: Terminal = self.terminal.clone().dyn_into().unwrap();
+    let term: Terminal = self.get_terminal();
     let event = e.dom_event();
     match event.key_code() {
       KEY_ENTER => {
@@ -245,67 +301,103 @@ impl WebRepl {
           && !event.ctrl_key()
           && !event.meta_key()
         {
-          let s = &event.key();
-          term.write(s);
-          line.push_str(s);
-          cursor_col += s.len();
-          self.history.pop_front();
-          self.history.push_front(line.clone());
+          // let s = &event.key();
+          // self.write(s);
         }
       }
     }
     self.save_history();
+    self.update_shell_state(ShellState { cursor_col, line, history_index });
+  }
+
+  pub fn read_shell_state(&self) -> ShellState {
+
+    let shell_state = self.shell_state.lock().unwrap();
+    shell_state.clone()
+  }
+
+  fn update_shell_state(&mut self, new_state: ShellState) {
     let mut shell_state = self.shell_state.lock().unwrap();
-    *shell_state = ShellState { cursor_col, line, history_index }
-  }
-}
-
-impl Repl for WebRepl {
-  fn readline(&mut self, _prompt: &str) -> Result<String, ReplError> {
-    Ok(self.shell_state.lock().unwrap().clone().line)
+    *shell_state = new_state;
   }
 
-  fn get_defs(&self) -> Arc<Mutex<Defs>> { self.defs.clone() }
+  pub fn handle_data(&mut self, data: String) {
+    let mut ss = self.read_shell_state();
+    let term: Terminal = self.get_terminal();
+    // log("got data");
+    log(&ss.line);
+    log(&format!("{:X?}", &data));
+    // term.write(&data);
 
-  fn get_store(&self) -> Rc<dyn Store> { self.store.clone() }
-
-  fn println(&self, s: String) { self.terminal.writeln(s.as_str()); }
-
-  fn load_history(&mut self) {
-    let window =
-      web_sys::window().expect("should have a window in this context");
-    let storage =
-      window.local_storage().expect("should have local storage").unwrap();
-    if let Ok(Some(text)) = storage.get("history.txt") {
-      let split: Vec<&str> = text.split("\n").collect();
-      for s in split {
-        self.history.push_front(s.to_owned());
+    match data.as_str() {
+      CURSOR_RIGHT => {
+        if ss.cursor_col < ss.line.len() {
+          term.write(CURSOR_RIGHT);
+          ss.cursor_col += 1;
+        }
       }
-      // Blank line is the current line
-      self.history.push_front("".to_owned());
-      log("History loaded");
+      CURSOR_LEFT => {
+        if ss.cursor_col > 0 {
+          term.write(CURSOR_LEFT);
+          ss.cursor_col -= 1;
+        }
+      } 
+      CURSOR_UP => {
+        if ss.history_index < self.history.len() - 1 {
+          ss.history_index += 1;
+          if let Some(l) = self.history.get(ss.history_index) {
+            ss.line = l.clone();
+            clear_line(&term);
+            term.write(PROMPT);
+            term.write(&ss.line);
+            ss.cursor_col = ss.line.len();
+          }
+        }
+      } 
+      CURSOR_DOWN => {
+        if ss.history_index > 0 {
+          ss.history_index -= 1;
+          if let Some(l) = self.history.get(ss.history_index) {
+            ss.line = l.clone();
+            clear_line(&term);
+            term.write(PROMPT);
+            term.write(&ss.line);
+            ss.cursor_col = ss.line.len();
+          }
+        }
+      }
+      BACKSPACE | "\u{7f}" => {
+        log("backspace");
+        if ss.cursor_col > 0 {
+          term.write("\u{0008} \u{0008}");
+          ss.line.pop();
+          ss.cursor_col -= 1;
+        }
+      } 
+      RETURN | LINEFEED | "\n" | "\r" => {
+        if !ss.line.is_empty() {
+          match self.handle_line(Ok(ss.line.clone())) {
+            Ok(()) => term.writeln("Ok"),
+            Err(()) => term.writeln("Error"),
+          }
+          ss.line.clear();
+          ss.cursor_col = 0;
+          ss.history_index = 0;
+        }
+        prompt(&term);
+      }
+      _ if data.starts_with("\x1b") => {
+        log("ESC");
+      } 
+      _ => {
+        term.write(&data);
+        ss.line.push_str(&data);
+        ss.cursor_col += data.len();
+        self.history.pop_front();
+        self.history.push_front(ss.line.clone());
+      }
     }
-    else {
-      log("Could not load history");
-    }
-  }
-
-  fn add_history_entry(&mut self, s: &str) { self.history.push_front(s.to_owned()); }
-
-  fn save_history(&mut self) {
-    let window =
-      web_sys::window().expect("should have a window in this context");
-    let storage =
-      window.local_storage().expect("should have local storage").unwrap();
-
-    let v: Vec<String> = self.history.clone().into();
-    let history = v.join("\n");
-    if let Ok(()) = storage.set("history.txt", &history) {
-      log("History saved");
-    }
-    else {
-      log("Could not save history");
-    }
+    self.update_shell_state(ss);
   }
 }
 
@@ -313,26 +405,31 @@ impl Repl for WebRepl {
 pub fn main() -> Result<(), JsValue> {
   utils::set_panic_hook();
 
-  let mut repl = WebRepl::new();
+  let repl_p = Arc::new(Mutex::new(WebRepl::new()));
+  let mut repl = repl_p.lock().unwrap();
   repl.load_history();
 
   let terminal: Terminal = repl.terminal.clone().dyn_into().unwrap();
 
+  let repl_p1 = repl_p.clone();
   let callback =
     Closure::wrap(
-      Box::new(move |e: OnKeyEvent| repl.handle_event(e)) as Box<dyn FnMut(_)>
+      Box::new(move |e: OnKeyEvent| {
+        let mut repl = repl_p1.lock().unwrap();
+        repl.handle_event(e)
+      }) as Box<dyn FnMut(_)>
     );
 
-  terminal.on_key(callback.as_ref().unchecked_ref());
+  // terminal.on_key(callback.as_ref().unchecked_ref());
 
   callback.forget();
 
+  let repl_p2 = repl_p.clone();
   let data_callback =
     Closure::wrap(
-      Box::new(move |data: Event| {
-        log("got data");
-        // log(data);
-        repl;
+      Box::new(move |data: String| {
+        let mut repl = repl_p2.lock().unwrap();
+        repl.handle_data(data);
       }) as Box<dyn FnMut(_)>
     );
 
