84b0daf03c33118f9e3826d8f619d2d716588356
Jeremy Schlatter <jeremy@jeremyschlatter.com>
2021-06-07 19:47:47 -0700

    update hash in nix/rust.nix
    Looks like this was missed when the default date changed in #53

--- nix/rust.nix
+++ nix/rust.nix
@@ -5,7 +5,7 @@
 let
   inherit channel date;
   targets = [ "wasm32-unknown-unknown" "wasm32-wasi" ];
-  sha256 = "HZIDqtsVmER8HlVTBTiuNFpLcX4jypueVpTLNutmIxk=";
+  sha256 = "N+G7d3+glt0O5n1yFRJdwFGg2xHRLl31YbxNRzwXP2w=";
   rust = (nixpkgs-mozilla.rustChannelOf {
     inherit channel date sha256;
   }).rust.override {
