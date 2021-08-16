be29f3adc17c4baf5ee88522458f66055a305b60
Elias Gabriel Amaral da Silva <tolkiendili@gmail.com>
2021-08-16 11:16:39 -0300

    Add npm run start:release and npm run build:release
    This will build the wasm code in release mode.
    
    With just npm start and npm run build, it builds in debug mode.

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,39 +1,37 @@
-const path = require('path');
+const path = require("path");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 
-module.exports = {
-  entry : "./bootstrap.js",
-  output : {
-    path : path.resolve(__dirname, "dist"),
-    filename : "bootstrap.js",
-  },
-  mode : "development",
-  plugins :
-    [
-      new CopyWebpackPlugin({ patterns : [ 'index.html' ], }),
-      new WasmPackPlugin(
-        {
-          crateDirectory: path.resolve(__dirname, '.'),
-          args: "--log-level warn",
+module.exports = function (env, argv) {
+  return {
+    entry: "./bootstrap.js",
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "bootstrap.js",
+    },
+    mode: "development",
+    plugins: [
+      new CopyWebpackPlugin({ patterns: ["index.html"] }),
+      new WasmPackPlugin({
+        crateDirectory: path.resolve(__dirname, "."),
+        args: "--log-level info",
 
-          // the mode `development` makes `wasm-pack` build in `debug` mode.
-          // the mode `production` makes `wasm-pack` build in `release` mode.
-          forceMode: "development",
-        }
-      ),
+        // the mode `development` makes `wasm-pack` build in `debug` mode.
+        // the mode `production` makes `wasm-pack` build in `release` mode.
+        forceMode: env.release ? "production" : "development",
+      }),
     ],
-  module : {
-    rules : [
-      {
-        test : /\.css$/i,
-        use : [ 'style-loader', 'css-loader' ],
-      },
-    ],
-  },
-  experiments:
-    {
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+      ],
+    },
+    experiments: {
       asyncWebAssembly: true,
       syncWebAssembly: true,
     },
+  };
 };
